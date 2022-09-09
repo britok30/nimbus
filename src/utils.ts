8d6ba487816eb248fe096365d06a0659c0ae65d8
Kelvin Brito <britok30@gmail.com>
2022-09-08 23:40:58 -0400

    add utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+export const capitalize = (string: string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export const ghostDownload = (filename: string, text: string) => {
+  const element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+  element.style.display = "none";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+};
